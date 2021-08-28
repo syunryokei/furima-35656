942c99b18f109220a22b1be202cf6b8a1ee27d7c
syunryokei <77194027+syunryokei@users.noreply.github.com>
2021-08-28 20:59:45 +0900

    インデントの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:product_name, :product_info, :price, :category_id, :condition_id, :postage_type_id,
-                                 :prefectures_id, :delivery_days_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :product_info, :price, :category_id, :condition_id, :postage_type_id,:prefectures_id, :delivery_days_id, :image).merge(user_id: current_user.id)
   end
 end
