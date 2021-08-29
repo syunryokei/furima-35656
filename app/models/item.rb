d3a3dc629418bfd5ca45ca51b3ccf0ba642700a5
syunryokei <77194027+syunryokei@users.noreply.github.com>
2021-08-29 09:35:00 +0900

    itemモデルバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,21 +3,24 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :postage_type
-  belongs_to :prefectures
-  belongs_to :delivery_days
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :postage_type
+    belongs_to :prefectures
+    belongs_to :delivery_days
+    
+    with_options presence: true do
+      validates :image
+      validates :product_name
+      validates :product_info
+      validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    end
 
-  with_options presence: true do
-    validates :image
-    validates :product_name
-    validates :product_info
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :category_id, numericality: { other_than: 1, message: 'カテゴリーを選択してください' }
-    validates :condition_id, numericality: { other_than: 1, message: '商品の状態を選択してください' }
-    validates :postage_type_id, numericality: { other_than: 1, message: '発送料の負担を選択してください' }
-    validates :prefectures_id, numericality: { other_than: 1, message: '発送元の地域を選択してください' }
-    validates :delivery_days_id, numericality: { other_than: 1, message: '発送までの日数を選択してください' }
+  with_options presence: true, numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_type_id
+    validates :prefectures_id
+    validates :delivery_days_id
   end
 end
