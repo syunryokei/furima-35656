c7d0b545d54304db7712edc3c6baf668376ee609
syunryokei <77194027+syunryokei@users.noreply.github.com>
2021-08-30 19:38:40 +0900

    商品詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show] do
+    resources :purchase_info, only: [:create]
+  end
+
 end
