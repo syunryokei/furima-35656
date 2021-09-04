9ddf80a710bec3f0da2461c9b842f681153f7c0f
syunryokei <77194027+syunryokei@users.noreply.github.com>
2021-09-04 10:28:02 +0900

    show,edit,updateアクションコードの共通化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :update, :show]
+  
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to root_path 
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,4 +43,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product_name, :product_info, :price, :category_id, :condition_id, :postage_type_id,
                                  :prefectures_id, :delivery_days_id, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
 end
