8de727ac894aa87bc0b8a09eb046e381c628b06e
syunryokei <77194027+syunryokei@users.noreply.github.com>
2021-09-04 17:43:15 +0900

    購入情報ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    # resources :purchase_info, only: [:create]
+     resources :purchase_info, only: [:create]
   end
 end
