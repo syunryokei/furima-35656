68154cecb8fc657727f339b62185eff0bfce0b37
syunryokei <77194027+syunryokei@users.noreply.github.com>
2021-09-22 20:35:48 +0900

    電話番号と購入品のバリデーション変更に伴うモデルテスト修正

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe PurchaseAddress, type: :model do
   before do
     user = FactoryBot.create(:user)
-    @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id)
+    item = FactoryBot.create(:item)
+    sleep 0.5
+    @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id)
   end
 
   context "内容に問題ない場合" do
@@ -56,6 +58,16 @@ RSpec.describe PurchaseAddress, type: :model do
       @purchase_address.phone_number = ''
       @purchase_address.valid?
       expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+    end
+    it 'phone_numberが10未満だと保存できないこと' do
+      @purchase_address.phone_number = '123456789'
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include("Phone number is too short (minimum is 10 characters)")
+    end
+    it 'phone_numberが11より大きいと保存できないこと' do
+      @purchase_address.phone_number = '123456789012'
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
     end
     it 'phone_numberが半角数字以外だと保存できないこと' do
       @purchase_address.phone_number = '０１００２０００３００'
